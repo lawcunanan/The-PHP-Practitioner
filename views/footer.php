1f74e008c6c80f4b65a2b374b2e164e4f40b6dfd
Lawrence Cunanan <enzocunanan01@gmail.com>
2025-11-20 12:08:02 +0800

    new update on footer

--- views/footer.php
+++ views/footer.php
@@ -1,6 +1,7 @@
     </main>
     <footer class="site-footer">
         <div class="container">
+            <p>waww</p>
             <p>&copy; <?php echo date('Y'); ?> Lawrence Ey</p>
         </div>
     </footer>
